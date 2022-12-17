62a36d025a7b5f8b1a61a5a57c5ec0524948db51
Hoangka12345 <hoangpbgch190102@gmail.com>
2022-12-17 18:37:53 +0700

    update url

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const request = axios.create({
-  // baseURL: "https://redux-server-nine.vercel.app/",
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://redux-server-nine.vercel.app/",
+  // baseURL: "http://localhost:5000/",
 });
 
 export default request;
